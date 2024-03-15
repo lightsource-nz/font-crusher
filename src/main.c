2b35f9988fa7b4a20c94395c2435bd61bd84fafc
tendrils <tendrils@users.noreply.github.com>
2024-03-15 17:03:03 +1300

    add dpi options

--- src/main.c
+++ src/main.c
@@ -4,18 +4,28 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define CODE_INVALID_ARG            1
+#include <ft2build.h>
+#include <freetype/freetype.h>
 
-static bool have_font_size;
-static bool have_out_filename;
-static uint8_t font_size;
-static char *out_filename;
+#define CODE_INVALID_ARG                1
+
+#define UNIT_PIXEL                      0
+#define UNIT_POINT                      1
+#define DPI_H_DEFAULT                   300
+#define DPI_V_DEFAULT                   300
+
+static uint8_t font_size = 0;
+static uint8_t font_size_unit = UNIT_PIXEL;
+static char *out_filename = NULL;
 static char *in_filename = NULL;
 static char *out_dirname = NULL;
+static uint16_t dpi_h = DPI_H_DEFAULT;
+static uint16_t dpi_v = DPI_V_DEFAULT;
 
 void print_usage();
 void set_option_font_size(char *value);
 void set_option_out_filename(char *value);
+void set_option_dpi(uint16_t val_dpi_h, uint16_t val_dpi_v);
 int main(int argc, char *argv[])
 {
         if(argc < 2) {
@@ -38,6 +48,11 @@ int main(int argc, char *argv[])
                         out_dirname = argv[i];
                 }
         }
+        if(!out_dirname) {
+                printf("Error: too few arguments (expected 2)\n");
+                print_usage();
+                return CODE_INVALID_ARG;
+        }
 
         return 0;
 }
@@ -50,12 +65,15 @@ void set_option_font_size(char *value)
                 printf("invalid number passed to option -s\n");
                 exit(CODE_INVALID_ARG);
         }
-        have_font_size = true;
 }
 void set_option_out_filename(char *value)
 {
         out_filename = value;
-        have_out_filename = true;
+}
+void set_option_dpi(uint16_t val_dpi_h, uint16_t val_dpi_v)
+{
+        dpi_h = val_dpi_h;
+        dpi_v = val_dpi_v;
 }
 void print_usage()
 {
